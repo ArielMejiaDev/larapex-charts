4503192561b9f9f81cee70f9c95a733f0b8656cc
Esdert Folkers <Edsardio@users.noreply.github.com>
2022-02-09 10:20:21 +0100

    Update LarapexChart.php

--- src/Facades/LarapexChart.php
+++ src/Facades/LarapexChart.php
@@ -2,15 +2,14 @@
 
 namespace ArielMejiaDev\LarapexCharts\Facades;
 
-use Illuminate\Support\Facades\Facade;
+use ArielMejiaDev\LarapexChart\LarapexChart;
 
 class LarapexChart extends Facade
 {
 
     protected static function getFacadeAccessor()
     {
-        static::clearResolvedInstance('larapex-chart');
-        return 'larapex-chart';
+        return LarapexChart::class;
     }
 
 }
